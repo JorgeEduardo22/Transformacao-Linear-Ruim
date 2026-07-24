e89a14cedf413be38568d97fa15ad640e6dd0b1d
Jorge Eduardo <jorgeeduardolapa138@gmail.com>
2026-07-24 18:32:53 -0300

    escalar foda (so os 2 eixos juntos ate agr)

--- script.js
+++ script.js
@@ -7,10 +7,34 @@
         let ligar = document.getElementById("ligar")
         let inputX = document.getElementById("X")
         let inputY = document.getElementById("Y")
+        let valorEscalar =document.getElementById("inputEscalarAmbos")
+        let escalar = document.getElementById("escalarAmbos")
         let matematica = create(all)
         let X = canva.width / 2
         let Y = canva.height / 2
+        let pontosOriginais = {
+            x: 0,
+            y: 0
+        }
         let angulo = 1
+        let animacao;
+        let velocidade = 1
+
+        function setPontoOriginal(){
+            pontosOriginais = setCoordenadas()
+        }
+
+        function setCoordenadas(x, y){
+            if (x !== undefined && y !== undefined){
+                inputX.value = x
+                inputY.value = y
+            }
+            return {
+                x: Number(inputX.value),
+                y: Number(inputY.value)
+            }
+        }
+
         function eixos(){
         ctx.beginPath()
             ctx.moveTo(canva.width / 2, 0)
@@ -30,18 +54,18 @@
                 [Math.cos(tethaRad), -(Math.sin(tethaRad))],
                 [Math.sin(tethaRad), Math.cos(tethaRad)]
             ]
-            let vetor = [[X], [Y]]
+            let vetor = [[pontosOriginais.x], [pontosOriginais.y]] 
             let resultado = matematica.multiply(matrizRotacao, vetor).valueOf()
+            escalarAmbos(Number(valorEscalar.value),resultado) 
+            desenhar()
 
-            desenhar(resultado[0][0], resultado[1][0])
-
-            return resultado
+            return 0
         }
 
-        function desenhar(X, Y) {
+        function desenhar() {
             ctx.clearRect(0, 0, canva.width, canva.height)
-            let canvasX = canva.width / 2 + X
-            let canvasY = canva.height / 2 - Y
+            let canvasX = canva.width / 2 + setCoordenadas().x
+            let canvasY = canva.height / 2 - setCoordenadas().y
             eixos()
             ctx.beginPath()
             ctx.arc(canvasX, canvasY, 5, 0, Math.PI * 2)
@@ -49,7 +73,7 @@
             ctx.fill()
             if (espelhar.checked) {
                 ctx.beginPath()
-                ctx.arc((canva.width / 2 - X), (canva.height / 2 + Y), 5, 0, Math.PI * 2)
+                ctx.arc((canva.width / 2 - setCoordenadas().x), (canva.height / 2 + setCoordenadas().y), 5, 0, Math.PI * 2)
                 ctx.fillStyle = "blue"
                 ctx.fill()
                 ctx.stroke()
@@ -57,23 +81,20 @@
             if (ligar.checked){
                 ctx.beginPath()
                 ctx.moveTo(canvasX, canvasY)
-                ctx.lineTo((canva.width / 2 - X), (canva.height / 2 + Y))
+                ctx.lineTo((canva.width / 2 - setCoordenadas().x), (canva.height / 2 + setCoordenadas().y))
                 ctx.stroke()
             }
-            
                 ctx.stroke()
-            
         }
 
+
         butao.addEventListener("click", () => {
-            X = Number(inputX.value)
-            Y = Number(inputY.value)
-            desenhar(X, Y)
+            setPontoOriginal()
+            desenhar()
         })
 
         range.addEventListener("input", () => {
-            X = Number(inputX.value)
-            Y = Number(inputY.value)
+           setCoordenadas()
             rotacionar(Number(range.value))
 
             document.getElementById("angulo").innerHTML = Number(range.value ) + "°"
@@ -83,24 +104,52 @@
 
 
         canva.addEventListener("click", () => {
-        console.log(event.offsetX, event.offsetX - 250 ,event.offsetY,  500 - event.offsetY)
-           desenhar(event.offsetX - 250 , 250 - event.offsetY )
+        
+        setCoordenadas((event.offsetX - 250), (250 - event.offsetY))
+        setPontoOriginal()
+        desenhar()
         })
 
        
         
 
        
-        setInterval(()=>{
-
-            if(document.getElementById("rotacionar").checked){
-        
+        function girar(){
+                    velocidade = Math.floor(Number(document.getElementById("velocidade").value))/10
                     if(angulo > 359){
                         angulo = 1
                      }
                     rotacionar(angulo)
-                        angulo++
-       }}, 1)
+                        angulo += velocidade
+         
+            animacao = requestAnimationFrame(girar)
+            }
+        function escalarAmbos(k, mtz){
+            let escalarAmbos = [[k,0],
+                                [0,k]]
+            let vetor = [[mtz[0][0]], [mtz[1][0]]]
+                
+                let resultado = matematica.multiply(escalarAmbos, vetor)
+                setCoordenadas(resultado[0][0], resultado[1][0])
+                desenhar()
+            
+        }
+   
+
+    document.getElementById("rotacionar").addEventListener("change", () => {
+        if (document.getElementById("rotacionar").checked){
+             document.getElementById("velocidade").style.display = "block"
+             document.getElementById("brvelocidade").style.display = "block"
+             
+            girar()
+        }
+
+        else{
+            cancelAnimationFrame(animacao)
+            document.getElementById("velocidade").style.display = "none"
+            document.getElementById("brvelocidade").style.display = "none"
+        }
+    })
         espelhar.addEventListener("change", () => {
 
         if(espelhar.checked){
@@ -116,4 +165,20 @@
                 
 
         }})
+
+            valorEscalar.addEventListener("input", () =>{
+            escalarAmbos(Number(valorEscalar.value), [[pontosOriginais.x], [pontosOriginais.y]])
+            })
+            
+            escalar.addEventListener("change", () => {
+                if(escalar.checked){
+                    document.getElementById("inputEscalarAmbos").style.display = 'block'
+                    document.getElementById("brEscalarAmbos").style.display = 'block'
+                }
+                else{
+                    document.getElementById("inputEscalarAmbos").style.display = 'none'
+                    document.getElementById("brEscalarAmbos").style.display = 'none'
+                }
+            })
+            
         eixos()
